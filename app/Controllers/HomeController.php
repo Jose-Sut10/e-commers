19b6eb7605efd4558178c198f5f333d6fc0b6584
Jose <jsut914@gmail.com>
2026-07-31 17:30:15 -0600

    estilizando el dashboard p1

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -8,8 +8,6 @@ use Core\View;
 class HomeController extends Controller
 {
     public function index(){
-        dump(config('app'));
-
         view('dashboard/index', [
             'title' => 'Dashboard'
         ]);
--- resources/views/dashboard/index.php
+++ resources/views/dashboard/index.php
@@ -1,3 +1,25 @@
 <h1>Dashboard</h1>
+<br>
 
-<p>Bienvenido al panel administrativo.</p>
+<div class="cards">
+    <div class="card">
+        <h3>Productos</h3>
+        <h1>0</h1>
+    </div>
+
+    <div class="card">
+        <h3>Clientes</h3>
+        <h1>0</h1>
+    </div>
+
+    <div class="card">
+        <h3>Pedidos</h3>
+        <h1>0</h1>
+    </div>
+
+    <div class="card">
+        <h3>Ventas</h3>
+        <h1>Q 0.00</h1>
+    </div>
+
+</div>
