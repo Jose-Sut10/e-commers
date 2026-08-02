abb8847d0c3f6dd2de2a343750cb3c997ab36299
Jose <jsut914@gmail.com>
2026-08-02 10:01:09 -0600

    agregando formulario de configuración de empresa

---
+++ resources/views/company/form.php
@@ -0,0 +1,43 @@
+<form method="POST">
+
+    <label>Nombre de la empresa</label>
+
+    <input
+        type="text"
+        name="name"
+    >
+
+    <br><br>
+
+    <label>Nombre comercial</label>
+
+    <input
+        type="text"
+        name="trade_name"
+    >
+
+    <br><br>
+
+    <label>NIT</label>
+
+    <input
+        type="text"
+        name="tax_id"
+    >
+
+    <br><br>
+
+    <label>Correo</label>
+
+    <input
+        type="email"
+        name="email"
+    >
+
+    <br><br>
+
+    <button>
+        Guardar
+    </button>
+
+</form>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Controllers\HomeController;
 use App\Controllers\EmpresaController;
+use App\Controllers\CompanyController;  
 
 $router->get('/', [
     HomeController::class,
@@ -11,4 +12,19 @@ $router->get('/', [
 $router->get('/empresa', [
     EmpresaController::class,
     'index'
+]);
+
+$router->get('/company', [
+    CompanyController::class,
+    'index'
+]);
+
+$router->get('/company/create', [
+    CompanyController::class,
+    'create'
+]);
+
+$router->post('/company', [
+    CompanyController::class,
+    'store'
 ]);
