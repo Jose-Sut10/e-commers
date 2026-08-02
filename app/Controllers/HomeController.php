7f57b528ba45b607cc553c41faa57b7aade99844
Jose <jsut914@gmail.com>
2026-08-02 10:39:26 -0600

    probando el funcionamiento de las validaciones

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,13 +3,26 @@
 namespace App\Controllers;
 
 use Core\Controller;
-use Core\View;
 
 class HomeController extends Controller
 {
-    public function index(){
-        view('dashboard/index', [
-            'title' => 'Dashboard'
-        ]);
+    public function index()
+    {
+        $result = validator(
+            [
+                'name'  => 'Jo',
+                'email' => 'correo-invalido',
+                'tax'   => 'abc',
+            ],
+            [
+                'name'  => 'required|min:3|max:150',
+                'email' => 'required|email',
+                'tax'   => 'numeric|min:0|max:100',
+            ]
+        )->validate();
+
+        echo '<pre>';
+        print_r($result->errors());
+        echo '</pre>';
     }
 }
